b20b1c166d148c1002ae96fc564d1246cf75d31f
Edmond Mbadu <mbadungoma@gmail.com>
2025-11-01 23:05:51 -0700

    updated saved to bookmarked collections

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -43,7 +43,7 @@ export class SidebarComponent {
       route: '/collections'
     },
     {
-      label: 'Bookmarked Collections',
+      label: 'Saved Collections',
       icon: 'bookmark',
       route: '/collections/bookmarked'
     }
